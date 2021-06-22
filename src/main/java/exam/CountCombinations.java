9bfe0807a4a7759e656a416d958cc1901882a78e
shubham <www.bandshubham015@gmail.com>
2021-06-22 12:58:19 +0530

    first commit

---
+++ src/main/java/exam/CountCombinations.java
@@ -0,0 +1,31 @@
+package exam;
+
+public class CountCombinations {
+    
+  public int getNumberOfWays(final int total, final int k) {
+    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+  
+    int count=0;
+    int s=0;
+    int flag=0;
+    
+    for(int i=1;i<=k;i++)
+    {
+        flag=0;
+        count=i;
+        while(count!=total)
+        {
+            count=count+1;
+            flag=1;
+        }
+        if(flag==1)
+        {
+            count++;
+        }
+        
+    }
+    
+    return count+1;
+  }
+
+}
